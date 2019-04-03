454b803d97486fb19ea7221c38c9aef5e63f20c2
Anjayjha99 <49182486+Anjayjha99@users.noreply.github.com>
2019-04-03 16:52:55 +0530

    Add files via upload

---
+++ twinkle.py
@@ -0,0 +1,5 @@
+print("Twinkle, Twinkle, Little Star,")
+print("\t How I wonder what you are")
+print("\t \t Up above the world so high")
+print("\t \t \t Like a diamond in the sky")
+print("Twinkle, Twinkle, Little star")
