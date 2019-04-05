4fa1f5f51e965fed0621ef9a2a5e68187d7f5659
Anjayjha99 <49182486+Anjayjha99@users.noreply.github.com>
2019-04-05 10:20:31 +0530

    Add files via upload

---
+++ name2.py
@@ -0,0 +1,12 @@
+name = input("Enter your name")
+print(name[1])
+print(name[-2])
+print(name[1:-1])
+print(len(name))
+x = (2+2) + (10%3)
+print(x)
+print(name.title())
+print(name.strip())
+print(name.find("a"))
+
+
